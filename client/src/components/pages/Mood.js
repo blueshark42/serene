ecfd7f040982186f50d1abcb5f2edc416657a224
PersephoneX <72812058+PersephoneX@users.noreply.github.com>
2021-02-21 23:33:27 +0530

    Update Mood.js

--- client/src/components/pages/Mood.js
+++ client/src/components/pages/Mood.js
@@ -84,26 +84,31 @@ export default class Mood extends Component {
                 width="300"
                 height="400"/>
             </div>
+          <p> Glad to know you are doing great! If you feel like it, feel free to leave a positive message for those who have not had a good day! </p>
             <div id="showMeSad">
                 <img src="https://mail.google.com/mail/u/0?ui=2&ik=10fdc34691&attid=0.1&permmsgid=msg-f:1692319290736972217&th=177c536dbaeb11b9&view=att&disp=safe&realattid=f_klfb3fyq1"
                 width="300"
                 height="400"/>
             </div>
+           <p> I am really sorry about the fact that your day did not go as well as planned. Do not worry, not every day will be this bad and in order to stay positive, we need to keep going! Read some of the positive messages if you think it will make you feel better! <\p>
             <div id="showMeTired">
                 <img src="https://mail.google.com/mail/u/0?ui=2&ik=10fdc34691&attid=0.2&permmsgid=msg-f:1692319290736972217&th=177c536dbaeb11b9&view=att&disp=safe&realattid=f_klfb3fyz2"
                 width="450"
                 height="400"/>
             </div>
+          <p> Some days are extremely busy and tiresome, and today seems to have been that kind of a day. Working hard will only make you successful in the future, so remember that your work will pay off soon! </p>
             <div id="showMeExcited">
                 <img src="https://mail.google.com/mail/u/0?ui=2&ik=10fdc34691&attid=0.3&permmsgid=msg-f:1692319290736972217&th=177c536dbaeb11b9&view=att&disp=safe&realattid=f_klfb3fy20"
                 width="300"
                 height="400"/>
             </div>
+          <p> We are pleased to know that you are excited! Share your excitement and happiness with those around you and spread the positivity! </p>
             <div id="showMeAnnoyed">
                 <img src="https://mail.google.com/mail/u/0?ui=2&ik=10fdc34691&attid=0.1&permmsgid=msg-f:1692319290736972217&th=177c536dbaeb11b9&view=att&disp=safe&realattid=f_klfb3fyq1"
                 width="300"
                 height="400"/>
             </div>
+           <p> If someone/something has bothered you today, try your best to calm yourself down. Remember that you can not control your surroundings, but you can control yourself by staying positive! </p>
         </div>
     );
   }
